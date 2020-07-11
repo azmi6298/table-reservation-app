24b131a2b1908fe2077dd46657cced4b4d282505
azmi6298 <azmi.filkom@gmail.com>
2020-07-11 19:51:55 +0700

    Minor fix on navbar router

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-blue-600 px-48 py-6 mb-16 w-full flex justify-between text-xl font-bold">
     <div>
-      <router-link to="">Table Reservation App</router-link>
+      <router-link to="/">Table Reservation App</router-link>
     </div>
     <div>
       <router-link to="/">Order Page</router-link>
